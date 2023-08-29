5f14fe7eefcddc79dd476a44796b52d379921b1d
Milandasmohapatra <sipudasmohapatra39@gmail.com>
2023-08-29 16:15:15 +0530

    colorchange

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -7,7 +7,7 @@ export default function ContactComponent(){
         <div className="row">
             <div className="col-8" style={{"marginLeft":"-40px"}}>
             <h1 align="center">About us</h1>
-            <h2 className="text-danger">We are a 21st Century IT company with all Modern Strategies</h2>
+            <h2 className="text-primary">We are a 21st Century IT company with all Modern Strategies</h2>
             
             <p>Miraki Tech is an expert in SaaS Development, Web Designing, Web Development, Mobile Application Development, Digital Marketing, Cloud Solutions, and many more all-round IT solutions serving across countries on the customers’ customized wants which helps them succeed in their business targets. We are committed to giving the stablest web solutions to all classes of businesses at an affordable range. We understand the uniqueness of your business and your business needs.</p>
             <div>
